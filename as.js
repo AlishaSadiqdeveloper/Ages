c9bd24f1419c28269d5783db35622f84226516fe
AlishaSadiqdeveloper <alishasadiq21@gmail.com>
2025-04-26 16:34:11 +0500

    Add files via upload

---
+++ as.js
@@ -0,0 +1,26 @@
+let Student1 =parseInt(prompt("First student age"));
+console.log ("First student");
+let Student2=parseInt(prompt("Second student age"));
+console.log ("second student");
+let Student3=parseInt(prompt("Third student age"));
+console.log ("third student");
+let Student4=parseInt(prompt("Fourth student age"));
+console.log ("fourth student");
+let Student5=parseInt(prompt("Fifth student age"));
+console.log ("fifth student");
+let Student6=parseInt(prompt("Sixth student age"));
+console.log ("six student");
+let Student7=parseInt(prompt("Seventh student age"));
+console.log ("seven student");
+let Student8=parseInt(prompt("Eightth student age"));
+console.log ("eight student");
+let Student9=parseInt(prompt("Nineth student age"));
+console.log ("nineth student");
+let Student10=parseInt(prompt("Tenth student age"));
+console.log ("ten student");
+let AverageAge =parseInt((Student1+Student2+Student3+Student4+Student5+Student6+Student7+Student8+Student9+Student10/10));
+console.log(AverageAge);
+
+
+
+
